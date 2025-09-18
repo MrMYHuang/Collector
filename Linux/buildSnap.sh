3dba906c9e11448d12de59035cf2df4723ee64a6
Meng-Yuan Huang <myh@live.com>
2025-09-18 09:59:07 +0800

    * use docker to build snap.

--- Linux/buildSnap.sh
+++ Linux/buildSnap.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+bundleVersion=$(cat ../ProjectSettings/ProjectSettings.asset|grep bundleVersion|awk '{print $2}')
+sed -i "" "s/^version: .*$/version: $bundleVersion/" ./snap/snapcraft.yaml
 chmod 777 snap/local/linux.x86_64
-snapcraft clean
-snapcraft
-sudo snap install game-collector_1.0.8_amd64.snap --dangerous
-snapcraft upload --release=stable game-collector_1.0.8_amd64.snap
+docker run --rm -it -v ./build:/build -v ./snap:/build/snap -w /build -v .snapcraft:/root/.snapcraft snapcore/snapcraft snapcraft --destructive-mode
+
+#snapcraft clean
+#snapcraft
+
+#sudo snap install game-collector_${bundleVersion}_amd64.snap --dangerous
+#snapcraft upload --release=stable game-collector_${bundleVersion}_amd64.snap
